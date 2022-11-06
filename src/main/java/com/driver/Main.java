c6b2a5928918842fe3b943cd5f676bb8cfefb3b9
Nan2812 <nancysinha1998@gmail.com>
2022-11-06 22:23:04 +0530

    added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,27 @@
 package com.driver;
 
 public class Main {
+    public static void main(String args[])
+    {
+        B obj = new B();
+        obj.meth();
+        obj.meth();
+
+    }
+    public static class A {
+        String meth() {
+            return "Invoking method from class A";
+        }
+    }
+        public static class B extends A {
+
+
+            String meth() {
+                return "Method is overridden in Extendend class B";
+            }
+
+
+        }
+
+    }
   
-}
